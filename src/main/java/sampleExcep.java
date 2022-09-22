3e4d95b27d6b9d5f8865faefd3b784ddc3f06248
SHILPA-GRACE-V <sgv3009@gmail.com>
2022-09-22 19:20:24 +0530

    Commit

---
+++ src/main/java/sampleExcep.java
@@ -0,0 +1,24 @@
+public class sampleExcep {
+    public static void main(String[] args) {
+        try{
+            int a=45,b=0;
+            int c=a/b;
+            System.out.println("result "+c);
+        }catch (ArithmeticException e){
+            System.out.println("can't divide by zero");
+
+        }
+        try {
+            int num=Integer.parseInt("sneha");
+            System.out.println(num);
+        }catch (NumberFormatException e){
+            System.out.println("Number format exception");
+        }
+        try{
+            int a[]=new int[5];
+            a[7]=9;
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(" array index out of bounds");
+        }
+    }
+}
